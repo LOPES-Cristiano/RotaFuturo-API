e03341e8ed8161ed619ee632d9f254af7c3e05ef
LOPES-Cristiano <cristianoc.lopes@hotmail.com>
2025-08-16 10:51:20 -0400

    feat: inicia spring

---
+++ src/main/java/br/com/rotafuturo/carreiras/RotafuturoApplication.java
@@ -0,0 +1,13 @@
+package br.com.rotafuturo.carreiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotafuturoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotafuturoApplication.class, args);
+	}
+
+}
